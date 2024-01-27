58cf079564123d6c18b69434797cc5424694c257
chinmay-saraswat <132015569+chinmay-saraswat@users.noreply.github.com>
2024-01-28 03:09:31 +0530

    Time: 4 ms (94.19%) | Memory: 19.6 MB (5.13%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,7 +4,28 @@ public:
         int n=nums.size();
         unordered_map<int, int> cnt;
         vector<int> ans;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){class Solution {
+public:
+    std::vector<int> majorityElement(std::vector<int>& nums) {
+        std::vector<int> result; 
+        std::unordered_set<int> uniqueNums;
+        for (int num : nums) {
+            uniqueNums.insert(num);
+        } 
+        for (int num : uniqueNums) {
+            int count = 0;
+            for (int n : nums) {
+                if (n == num) {
+                    count++;
+                }
+            }
+            if (count > nums.size() / 3) {
+                result.push_back(num);
+            }
+        } 
+        return result;
+    }
+};
             cnt[nums[i]]++;
         }
         for(auto j=cnt.begin();j!=cnt.end();j++){
