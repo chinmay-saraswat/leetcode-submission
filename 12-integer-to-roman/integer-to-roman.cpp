2944d85e2c71a6dc48e4601f102b532bb222d72d
chinmay-saraswat <132015569+chinmay-saraswat@users.noreply.github.com>
2024-03-23 20:55:40 +0530

    Time: 17 ms (7.82%) | Memory: 16.2 MB (8.29%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
         
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        // pre calculate the roman value
+        
+        vector<string> ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        
+        vector<string> tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        
+        vector<string> hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        
+        vector<string> thousands = {"", "M", "MM", "MMM"};
+        
+        string res = "";
+        
+        // find digit at thousands place
+        
+        res += thousands[num / 1000];
+        
+        // find digit at hundreds place
+        
+        res += hundreds[(num % 1000) / 100];
+        
+        // find digit at tens place
+        
+        res += tens[(num % 100) / 10];
+        
+        // find digit at ones place
+        
+        res += ones[num % 10];
+        
+        return res;
     }
 };
